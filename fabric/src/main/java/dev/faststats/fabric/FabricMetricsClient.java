f75668beb43cd136473665d626d42a942c708a7f
david <mail@nonswag.dev>
2026-06-04 20:25:49 +0200

    Replace client lifecycle with direct getter

--- fabric/src/main/java/dev/faststats/fabric/FabricMetricsClient.java
+++ fabric/src/main/java/dev/faststats/fabric/FabricMetricsClient.java
@@ -1,29 +1,24 @@
 package dev.faststats.fabric;
 
 import com.google.gson.JsonObject;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.client.Minecraft;
-import org.jspecify.annotations.Nullable;
 
 final class FabricMetricsClient extends FabricMetrics {
-    private @Nullable Minecraft client;
+    private final Minecraft client = Minecraft.getInstance();
 
     public FabricMetricsClient(final Factory factory, final ModContainer mod) throws IllegalStateException {
         super(factory, mod);
-        ClientLifecycleEvents.CLIENT_STARTED.register(client -> this.client = client);
     }
 
     @Override
     protected void appendDefaultData(final JsonObject metrics) {
-        assert client != null : "Client not initialized";
         metrics.addProperty("online_mode", client.getUser().getXuid().isPresent() && !client.isOfflineDeveloperMode()); // todo: doublecheck
         metrics.addProperty("player_count", getPlayerCount());
         appendFabricData(metrics, "Fabric Client");
     }
 
     private int getPlayerCount() {
-        assert client != null : "Client not initialized";
         final var connection = client.getConnection();
         if (connection != null) return connection.getOnlinePlayers().size();
 
